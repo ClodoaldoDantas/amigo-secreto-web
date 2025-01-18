d50004019a893ee28812f0fd1d847c48234820af
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-01-18 11:14:24 -0300

    feat: change font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,9 @@ export default {
           5: "hsl(var(--chart-5))",
         },
       },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
