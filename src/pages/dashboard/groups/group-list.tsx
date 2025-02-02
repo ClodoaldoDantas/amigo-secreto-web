6470c62c50846c410a544e4185b5074513e1439d
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2025-02-02 14:47:02 -0300

    feat: show message if group list is empty

--- src/pages/dashboard/groups/group-list.tsx
+++ src/pages/dashboard/groups/group-list.tsx
@@ -1,4 +1,6 @@
 import type { FetchGroupsResponse } from '@/api/fetch-groups'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { AlertCircle } from 'lucide-react'
 import { use } from 'react'
 import { GroupCard } from './group-card'
 
@@ -10,10 +12,20 @@ export function GroupList({ fetchGroupsPromise }: GroupListProps) {
 	const { groups } = use(fetchGroupsPromise)
 
 	return (
-		<div className="my-6 grid grid-cols-4 gap-3">
-			{groups.map((group) => (
-				<GroupCard key={group._id} group={group} />
-			))}
-		</div>
+		<>
+			{groups.length === 0 && (
+				<Alert className="my-6">
+					<AlertCircle className="h-4 w-4" />
+					<AlertTitle className="text-base">Nenhum grupo encontrado</AlertTitle>
+					<AlertDescription>Crie um novo grupo para começar.</AlertDescription>
+				</Alert>
+			)}
+
+			<div className="my-6 grid grid-cols-4 gap-3">
+				{groups.map((group) => (
+					<GroupCard key={group._id} group={group} />
+				))}
+			</div>
+		</>
 	)
 }
